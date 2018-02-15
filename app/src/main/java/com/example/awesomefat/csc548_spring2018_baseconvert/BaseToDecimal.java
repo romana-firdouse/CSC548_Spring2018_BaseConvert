537fcb3b53031f81f7c7427b7f2801a6b24c96da
romana <firdouse.romana@cuw.edu>
2018-02-15 16:44:21 -0600

    RomanaFcsc548HW#2

--- app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/BaseToDecimal.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/BaseToDecimal.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class BaseToDecimal extends AppCompatActivity
 {
@@ -21,23 +23,147 @@ public class BaseToDecimal extends AppCompatActivity
         this.answerTV = (TextView)this.findViewById(R.id.answerTV);
     }
 
+    private int mapCharToNum(char ch)
+    {
+        if (ch >= '0' && ch <= '9')
+            return ch - '0';
+
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            return ch - 'A' + 10;
+        }
+        return ch - 'a' + 10;
+    }
+
+    private int convertBaseToDecimal(int base, String numStr)
+    {
+        int len = numStr.length();
+
+        int val = 0;
+
+        for (int i = 0; i < len; i++)
+        {
+            char ch = numStr.charAt(i);
+            int n = 0;
+
+            if (ch >= '0' && ch <= '9')
+            {
+                n = ch - 48;
+            }
+            else if (ch >= 'A' && ch <= 'Z')
+            {
+                n = ch - 'A' + 10;
+            }
+            else if (ch >= 'a' && ch <= 'z')
+            {
+                n = ch - 'a' + 10;
+            }
+
+            val = val  * base +  n;
+        }
+
+        return val;
+    }
+
+    private boolean isValidBaseNumber(int base, String numStr)
+    {
+        int len = numStr.length();
+
+        for (int i = 0; i < len; i++)
+        {
+            char ch = numStr.charAt(i);
+
+            if (!(ch >= '0' && ch <= '9') &&
+                    !(ch >= 'A' && ch <= 'Z') &&
+                    !(ch >= 'a' && ch <= 'z'))
+            {
+                return false;
+            }
+
+            if (mapCharToNum(ch) >= base)
+                return false;
+        }
+        return true;
+    }
+
     public void base2ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 2");
+        if (this.inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+
+        String num = inputET.getText().toString();
+
+        if (!isValidBaseNumber(2, num))
+        {
+            Toast.makeText(BaseToDecimal.this, "Invalid number",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        answerTV.setText("" + convertBaseToDecimal(2, num));
+
     }
 
     public void base8ButtonPressed(View v)
+
     {
-        this.answerTV.setText("Base 8");
+        if (this.inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+
+        String num = inputET.getText().toString();
+
+        if (!isValidBaseNumber(8, num))
+        {
+            Toast.makeText(BaseToDecimal.this, "Invalid number",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        answerTV.setText("" + convertBaseToDecimal(8, num));
     }
 
     public void base10ButtonPressed(View v)
+
     {
-        this.answerTV.setText("Base 10");
+        if (this.inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+
+        String num = inputET.getText().toString();
+
+        if (!isValidBaseNumber(10, num))
+        {
+            Toast.makeText(BaseToDecimal.this, "Invalid number",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        answerTV.setText("" + convertBaseToDecimal(10, num));
     }
 
     public void base16ButtonPressed(View v)
+
     {
-        this.answerTV.setText("Base 16");
+        if (this.inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+
+        String num = inputET.getText().toString();
+
+        if (!isValidBaseNumber(16, num))
+        {
+            Toast.makeText(BaseToDecimal.this, "Invalid number",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        answerTV.setText("" + convertBaseToDecimal(16, num));
     }
+
 }
--- app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/DecimalToBase.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/DecimalToBase.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class DecimalToBase extends AppCompatActivity
 {
@@ -22,23 +24,74 @@ public class DecimalToBase extends AppCompatActivity
         this.answerTV = (TextView)this.findViewById(R.id.answerTV);
     }
 
+    private String convertDecimalToBaseNumber(String numStr, int base)
+    {
+        int  num = Integer.parseInt(numStr);
+
+        String baseNum = "";
+        String rem = "";
+
+        while (num > 0)
+        {
+            if ((num % base) <= 9)
+                rem = Integer.toString(num % base);
+            else
+                rem = Character.toString((char)('A' + (num % base) - 10));
+
+            baseNum = rem + baseNum;
+            num = num / base;
+        }
+
+        if (baseNum == "") /* To handle a case where input is zero*/
+            return "0";
+
+        return baseNum;
+    }
+
+
     public void base2ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 2");
+        if (inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+        String num = inputET.getText().toString();
+        String val = convertDecimalToBaseNumber(num, 2);
+        answerTV.setText("Base " + num + " :  " + val);
+
     }
 
     public void base8ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 8");
+        if (inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+        String num = inputET.getText().toString();
+        String val = convertDecimalToBaseNumber(num, 8);
+        answerTV.setText("Base " + num + " :  " + val);
     }
 
     public void base10ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 10");
+        if (inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+        String num = inputET.getText().toString();
+        String val = convertDecimalToBaseNumber(num, 10);
+        answerTV.setText("Base " + num + " :  " + val);
     }
 
     public void base16ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 16");
+        if (inputET.getText().toString().equals(""))
+        {
+            return;
+        }
+        String num = inputET.getText().toString();
+        String val = convertDecimalToBaseNumber(num, 16);
+        answerTV.setText("Base " + num + " :  " + val);
     }
+
 }
